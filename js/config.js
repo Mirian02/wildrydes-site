87e7acdc2bde795fa3790470957976bb4e847c76
Mirian Ejechi <118598344+Mirian02@users.noreply.github.com>
2023-06-24 10:36:11 +0100

    updated the poolID and Client ID

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_BDIX3AEs6', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '23ns9fpnod6jq8d5dvm0topemv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1gi' // e.g. us-east-2
+        userPoolId: 'us-east-1_UaE26NUOz', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4jqut3p62te3a77ckoul2vrtf2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://jleiger2ek.execute-api.us-east-1.amazonaws.com/prod' // e.g. ',
+        invokeUrl: 'https://9lsu2u005k.execute-api.us-east-1.amazonaws.com/prod' // e.g. ',
     }
-};
+}
